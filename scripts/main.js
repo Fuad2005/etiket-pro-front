529262a9b242612c4e6e254e74e781c9cfeee056
BayramMusayev <bayrammusayev1992@gmail.com>
2024-06-06 15:15:19 +0400

    home added

--- scripts/main.js
+++ scripts/main.js
@@ -31,6 +31,29 @@ function toggleList(listId) {
 }
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    const counters = document.querySelectorAll('.count');
+    const speed = 200; // Daha aşağı rəqəm daha yavaş countır
+
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-count');
+            const count = +counter.innerText;
+            const increment = target / speed;
+
+            if (count < target) {
+                counter.innerText = Math.ceil(count + increment);
+                setTimeout(updateCount, 10);
+            } else {
+                counter.innerText = target;
+            }
+        };
+
+        updateCount();
+    });
+});
+
+
 
 
 
